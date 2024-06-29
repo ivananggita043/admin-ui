62f84e928c5a118a3216f8ecec9b5cd1c2ceca8b
Ivan Maulana Anggita <62920144+ivananggita043@users.noreply.github.com>
2024-06-29 11:23:44 +0700

    read delete data

--- src/pages/single/Single.jsx
+++ src/pages/single/Single.jsx
@@ -3,9 +3,14 @@ import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
 import Chart from "../../components/chart/Chart";
 import List from "../../components/datatable/Datatable";
+import Datatable from "../../components/datatable/Datatable";
+import { useLocation } from "react-router-dom";
 
 
-const Single = () => {
+const Single = ({columns}) => {
+  const location = useLocation();
+  const id = location.pathname.split('/')[2];
+
   return (
     <div className="single">
       <Sidebar />
@@ -23,6 +28,10 @@ const Single = () => {
                 />
               <div className="details">
                 <h1 className="itemTitle">Jane Doe</h1>
+                <div className="detailItem">
+                  <span className="itemKey">ID:</span>
+                  <span className="itemValue">{id}</span>
+                </div>
                 <div className="detailItem">
                   <span className="itemKey">Email:</span>
                   <span className="itemValue">janedoe@gmail.com</span>
@@ -50,7 +59,7 @@ const Single = () => {
         </div>
         <div className="bottom">
           <h1 className="title">Last Transactions</h1>
-          <List />
+          <Datatable columns={columns}/>
         </div>
       </div>
     </div>
